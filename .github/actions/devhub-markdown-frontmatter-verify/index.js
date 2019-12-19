459ae5dcfc9f0727d59d155e45d1c60d6f462245
Patrick Simonian <Patrick.Simonian@gov.bc.ca>
2019-12-19 13:26:52 -0800

    debug

--- .github/actions/devhub-markdown-frontmatter-verify/index.js
+++ .github/actions/devhub-markdown-frontmatter-verify/index.js
@@ -9,7 +9,7 @@ async function run() {
     // The YML workflow will need to set myToken with the GitHub Secret Token
     // myToken: ${{ secrets.GITHUB_TOKEN }}
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const myToken = core.getInput('myToken');
+    // const myToken = core.getInput('myToken');
     // const octokit = new github.GitHub(myToken);
     const context = github.context;
 
